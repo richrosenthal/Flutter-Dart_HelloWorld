55d04143253c8933dfffc383146d6375ffb24210
Richard <42520886+richrosenthal@users.noreply.github.com>
2019-08-26 20:12:04 -0400

    Add files via upload

---
+++ HelloWorld.dart
@@ -0,0 +1,17 @@
+//Author Richard Rosenthal
+//Date: 8-26-19
+//Just a simple "Hello World" program with a twist
+//This is the start of my Flutter/Dart journey.
+
+import 'package:flutter/material.dart';
+
+void HelloWorld() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        //Centers the text
+        child: Text('Hiya Georgie!'),
+      ),
+    ),
+  );
+}
